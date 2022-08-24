4e3a4df2f629b340e4b7c3bcf106b78400766b34
Mr.M.V.BALA <mvbalakrishnan95@gmail.com>
2022-08-24 20:28:44 +0530

    It is an udemy exercise.

--- src/GreatestCommonDivisor.java
+++ src/GreatestCommonDivisor.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class GreatestCommonDivisor {
+public class GreatestCommonDivisor {
+    public static int getGreatestCommonDivisor(int first,int second) {
+        if(first<10 || second<10)
+            return -1;
+        else {
+            int gcd=1;
+            for(int i=2;i<=first&& i<=second;i++){
+                if(first%i==0 &&second%i==0){
+                    gcd=i;
+                }
+            }
+            return gcd;
+        }
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(GreatestCommonDivisor.getGreatestCommonDivisor(22, 11));
+        System.out.println(GreatestCommonDivisor.getGreatestCommonDivisor(25, 15));
+        System.out.println(GreatestCommonDivisor.getGreatestCommonDivisor(2, 11));
+        System.out.println(GreatestCommonDivisor.getGreatestCommonDivisor(882, 11));
+        System.out.println(GreatestCommonDivisor.getGreatestCommonDivisor(12, 30));
     }
 }
